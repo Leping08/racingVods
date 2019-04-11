b13974a9bdaea7b873a3546b62eaedc166fa909f
Leping08 <dereks008@gmail.com>
2019-04-11 12:54:46 -0400

    Add new attrbuite to the race model and add php docs to the race model

--- app/Race.php
+++ app/Race.php
@@ -2,8 +2,30 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Nova\Actions\Actionable;
+
+/**
+ * An Eloquent Model: 'Race'
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $season_id
+ * @property integer $series_id
+ * @property integer $track_id
+ * @property string $duration
+ * @property bool $new
+ * @property \Illuminate\Support\Carbon race_date
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property-read Series $series
+ * @property-read Track $track
+ * @property-read Season $season
+ * @property-read Video $videos
+ */
 
 class Race extends Model
 {
@@ -17,38 +39,69 @@ class Race extends Model
         'race_date' => 'datetime:Y-m-d',
     ];
 
-    protected $fillable = ['name', 'race_date', 'season_id', 'series_id', 'track_id', 'youtube_id', 'youtube_start_time', 'duration'];
+    protected $fillable = ['name', 'race_date', 'season_id', 'series_id', 'track_id', 'duration'];
 
+    /**
+     * @return BelongsTo
+     */
     public function series()
     {
         return $this->belongsTo(Series::class);
     }
 
+    /**
+     * @return BelongsTo
+     */
     public function track()
     {
         return $this->belongsTo(Track::class);
     }
 
+    /**
+     * @return BelongsTo
+     */
     public function season()
     {
         return $this->belongsTo(Season::class);
     }
 
+    /**
+     * @return HasMany
+     */
     public function videos()
     {
         return $this->hasMany(Video::class);
     }
 
+    /**
+     * @return bool
+     */
+    public function getNewAttribute()
+    {
+        return $this->created_at > Carbon::now()->subDays(7);
+    }
+
+    /**
+     * @param $query
+     * @return Series
+     */
     public function scopeThisSeason($query)
     {
         return $query->where('season_id', $this->season->id);
     }
 
+    /**
+     * @param $query
+     * @return Series
+     */
     public function scopeThisSeries($query)
     {
         return $query->where('series_id', $this->series->id);
     }
 
+    /**
+     * @return Race
+     */
     public function next()
     {
         $collection = $this->thisSeason()
@@ -63,6 +116,9 @@ class Race extends Model
         return $collection->get($index + 1);
     }
 
+    /**
+     * @return Race
+     */
     public function previous()
     {
         $collection = $this->thisSeason()
