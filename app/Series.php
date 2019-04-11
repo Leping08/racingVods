07dd7459fb037405ef80b80b39861e86b0a9d2f9
Leping08 <dereks008@gmail.com>
2019-04-11 19:59:35 -0400

    Add php docs to series model

--- app/Series.php
+++ app/Series.php
@@ -2,8 +2,24 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Nova\Actions\Actionable;
+
+/**
+ * An Eloquent Model: 'Series'
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $fullName
+ * @property string $image
+ * @property string $website
+ * @property string $description
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read Race $races
+ */
 
 class Series extends Model
 {
@@ -12,6 +28,9 @@ class Series extends Model
     //TODO: Update column name of fullName
     protected $fillable = ['name', 'fullName', 'image', 'website', 'description'];
 
+    /**
+     * @return HasMany
+     */
     public function races()
     {
         return $this->hasMany(Race::class)->with('season')->orderBy('race_date', 'asc');
