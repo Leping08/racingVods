f2280dc0530f4029992141cb81e75ef21ed71981
Leping08 <dereks008@gmail.com>
2019-04-11 21:27:15 -0400

    Add php docs to track model

--- app/Track.php
+++ app/Track.php
@@ -2,8 +2,25 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Nova\Actions\Actionable;
+
+/**
+ * An Eloquent Model: 'Tracks'
+ *
+ * @property integer $id
+ * @property string $name
+ * @property float $length
+ * @property string $image
+ * @property string $website
+ * @property string $numberOfCorners
+ * @property string $description
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read Race $races
+ */
 
 class Track extends Model
 {
@@ -13,6 +30,9 @@ class Track extends Model
     //TODO: Update the name of the numberOfCorners column
     protected $fillable = ['name', 'length', 'image', 'website', 'numberOfCorners', 'description'];
 
+    /**
+     * @return HasMany
+     */
     public function races()
     {
         return $this->hasMany(Race::class)->orderBy('race_date');
