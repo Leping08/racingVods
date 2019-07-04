91ee0f08bf485750e39c001d47ec6f15c53a3f86
Leping08 <dereks008@gmail.com>
2019-07-03 22:13:17 -0400

    Fix naming conventions in router

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -5,10 +5,10 @@ Vue.use(VueRouter);
 
 const about = () => import('./components/about/about.vue');
 const home = () => import('./components/home/home.vue');
-const dashboard = () => import('./components/dashboard/dashboard');
+const dashboard = () => import('./components/dashboard/dashboard.vue');
 const notFound = () => import('./components/not-found/notFound.vue');
 
-const logout = () => import('./components/admin/logout');
+const logout = () => import('./components/admin/logout.vue');
 const login = () => import('./components/admin/login.vue');
 
 const races = () => import('./components/races/races.vue');
@@ -17,7 +17,7 @@ const createRace = () => import('./components/races/create.vue');
 
 const series = () => import('./components/series/series.vue');
 const seriesID = () => import('./components/series/seriesID.vue');
-const createSeries = () => import('./components/series/create');
+const createSeries = () => import('./components/series/create.vue');
 const seriesAdmin = () => import('./components/series/seriesAdmin.vue');
 
 const tracks = () => import('./components/tracks/tracks.vue');
@@ -25,9 +25,9 @@ const tracksID = () => import('./components/tracks/tracksID.vue');
 const createTrack = () => import('./components/tracks/create.vue');
 
 const seasonRaces = () => import('./components/season/seasonRaces.vue');
-const createVideo = () => import('./components/videos/create');
+const createVideo = () => import('./components/videos/create.vue');
 
-const schedule = () => import('./components/calendar/schedule');
+const schedule = () => import('./components/calendar/schedule.vue');
 
 const router = new VueRouter({
     mode: 'history',
