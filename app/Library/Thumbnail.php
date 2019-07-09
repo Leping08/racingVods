217e33bded77e89574d8f5135007272f07c7cbc8
Leping08 <dereks008@gmail.com>
2019-07-09 13:23:18 -0400

    Add thumbnail class that will vaildate a thumbnail for a video

--- app/Library/Thumbnail.php
+++ app/Library/Thumbnail.php
@@ -4,7 +4,38 @@
 namespace App\Library;
 
 
-class ValidateThumbnail
-{
+use App\Video;
+use GuzzleHttp\Client;
 
+
+class Thumbnail
+{
+    private $video;
+
+    public function __construct(Video $video)
+    {
+        $this->video = $video;
+    }
+
+    public function validate()
+    {
+        $client = new Client();
+
+        //TODO: Write a test for this class
+        try{
+            $res = $client->get($this->video->thumbnail);
+            if($res->getStatusCode() == 200){
+                $this->save(true);
+            } else {
+                $this->save(false);
+            }
+        } catch (\Exception $exception) {
+            $this->save(false);
+        }
+    }
+
+    private function save(bool $bool){
+        $this->video->valid_thumbnail = $bool;
+        $this->video->save();
+    }
 }
