780bc6b1df347f7d9625022c643b29a1d0bd6bd2
Leping08 <dereks008@gmail.com>
2019-09-08 16:07:08 -0400

    Add logic to guess the track of a potential race

--- app/Library/PotentialRaces/Track/GuessTrack.php
+++ app/Library/PotentialRaces/Track/GuessTrack.php
@@ -4,12 +4,55 @@
 namespace App\Library\PotentialRaces\Track;
 
 
+use App\Track;
+
 class GuessTrack
 {
-    public static function test()
+    public static function handle(string $string)
     {
-        //TODO Get track from youtube video
-        //Use text in race title
-        //Use ML to scan the video
+        //Search text in race title for track name
+        $string = strtolower($string);
+
+        // split on 1+ whitespace & ignore empty (eg. trailing space)
+        $searchValues = collect(preg_split('/\s+/', $string, -1, PREG_SPLIT_NO_EMPTY));
+
+        //Exclude these values from the search
+        $exclude = [
+            'to',
+            'the',
+            'of',
+            'at',
+            'st.',
+            'raceway',
+            'international',
+            'speedway',
+            'streets',
+            'fast',
+            'forward',
+            'forward:',
+            'grand',
+            'prix',
+            'motorsports',
+            'motorsport',
+            'motor',
+            'road',
+            'circuit'
+        ];
+
+        //Remove any strings that are in the $exclude array
+        $filteredValues = $searchValues->reject(function ($item) use ($exclude) {
+            return in_array($item, $exclude);
+        });
+
+        //Loop over tracks and search
+        $tracks = Track::where(function ($query) use ($filteredValues) {
+            foreach ($filteredValues as $value) {
+                $query->orWhere('name', 'like', "%{$value}%");
+            }
+        })->get();
+
+        //Return the first track id if the system found any
+        return $tracks->count() ? $tracks->first()->id : null;
+        //TODO Use ML to scan the video
     }
 }
--- app/Library/PotentialRaces/Youtube/Api.php
+++ app/Library/PotentialRaces/Youtube/Api.php
@@ -5,6 +5,7 @@ namespace App\Library\PotentialRaces\Youtube;
 
 
 use Alaouy\Youtube\Youtube;
+use App\Library\PotentialRaces\Track\GuessTrack;
 use App\Season;
 use App\Series;
 use Carbon\CarbonInterval;
@@ -26,6 +27,7 @@ class Api
 
         //Loop over the latest videos and return the video if it's longer then the min_race_time
         foreach ($newVideos['results'] as $vid) {
+            dd($vid);
             $ISOvidDuration = $youtube->getVideoInfo($vid->contentDetails->videoId)->contentDetails->duration;
             $carbonDuration = CarbonInterval::create($ISOvidDuration);
             $vidDurationInMin = $carbonDuration->__get('minutes') + (60 * $carbonDuration->__get('hours'));
@@ -33,7 +35,7 @@ class Api
             if ($vidDurationInMin >= $series->min_race_time) {
                 $vid->series_id = $series->id;
                 $vid->season_id = Season::current();
-                $vid->track_id = null; //TODO Get Track ID working
+                $vid->track_id = GuessTrack::handle($vid->snippet->title);
                 $potentialNewRaces->push($vid);
             }
         }
