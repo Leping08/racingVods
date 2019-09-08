fbfa44588aab08b8a757295721f3b5c93fa04d98
Leping08 <dereks008@gmail.com>
2019-09-07 21:11:20 -0400

    Update email to say no new races

--- resources/views/emails/newRacesReport.blade.php
+++ resources/views/emails/newRacesReport.blade.php
@@ -4,14 +4,18 @@
 *{{\Carbon\Carbon::now()->toFormattedDateString()}}*
 
 @component('mail::table')
-    | Title         | Youtube_id            | Link     |
-    |:------------- |:--------------------- |:-------- |
-    @foreach($races as $race)
-        | {{$race->title}}  | {{$race->youtube_id}} |  [Watch](https://www.youtube.com/watch?v={{$race->youtube_id}}) |
-    @endforeach
+    @if($races->count())
+        | Title         | Youtube_id            | Link     |
+        |:------------- |:--------------------- |:-------- |
+        @foreach($races as $race)
+            | {{$race->title}}  | {{$race->youtube_id}} |  [Watch](https://www.youtube.com/watch?v={{$race->youtube_id}}) |
+        @endforeach
+    @else
+        No new races today
+    @endif
 @endcomponent
 
-@component('mail::button', ['url' => '/create-potential-races', 'color' => 'green'])
+@component('mail::button', ['url' => '/nova/resources/potential-races', 'color' => 'green'])
 View Details
 @endcomponent
 
